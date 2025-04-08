b8ae60b19d21bc43e5b0eea2b44730ecc8084c5c
Alex. <plohih215@ya.ru>
2025-04-08 15:03:17 +0500

    change sub command

--- app/Entrypoints/TelegramCommands/SubCommand.php
+++ app/Entrypoints/TelegramCommands/SubCommand.php
@@ -13,28 +13,28 @@ class SubCommand extends Command
 
     public function handle()
     {
-        $subKey = $this->argument('subKey', $this->getUpdate()->getMessage()->from->subKey);
-        $messageInfo = $this->getUpdate()
-            ->getMessage()
-            ->toArray();
-        $chatId = $messageInfo['chat']['id'];
-
-        if ($subKey !== env('TELEGRAM_BOT_SUB_KEY')) {
-            $this->replyWithMessage([
-                'text'=> 'Ключ не валиден, подписка невозможна!'
-            ]);
-        } elseif (TelegramChats::where('chatId', $chatId)->exists()) {
-            $this->replyWithMessage([
-                'text' => 'Вы уже подписыны'
-            ]);
-        } else {
-            TelegramChats::create([
-                'chatId' => $chatId,
-            ]);
-
-            $this->replyWithMessage([
-                'text' => 'Вы подписались'
-            ]);
-        }
+//        $subKey = $this->argument('subKey', $this->getUpdate()->getMessage()->from->subKey);
+//        $messageInfo = $this->getUpdate()
+//            ->getMessage()
+//            ->toArray();
+//        $chatId = $messageInfo['chat']['id'];
+//
+//        if ($subKey !== env('TELEGRAM_BOT_SUB_KEY')) {
+//            $this->replyWithMessage([
+//                'text'=> 'Ключ не валиден, подписка невозможна!'
+//            ]);
+//        } elseif (TelegramChats::where('chatId', $chatId)->exists()) {
+//            $this->replyWithMessage([
+//                'text' => 'Вы уже подписыны'
+//            ]);
+//        } else {
+//            TelegramChats::create([
+//                'chatId' => $chatId,
+//            ]);
+//
+//            $this->replyWithMessage([
+//                'text' => 'Вы подписались'
+//            ]);
+//        }
     }
 }
